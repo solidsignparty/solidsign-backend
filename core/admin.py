bf660c0a74dd4c8e700821f3eb1301ce274be42b
Rustam Kashapov <hardtechnik91@gmail.com>
2024-04-12 14:46:49 +0300

    add display fields to admin

--- core/admin.py
+++ core/admin.py
@@ -5,5 +5,6 @@ from .models import Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'start_time', 'location')
     search_fields = ('title', 'location')
     date_hierarchy = 'start_time'
